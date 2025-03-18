7c05be3941f01c2b05c56775cc822ac6cbbeeb0d
Hiep Tran <hiep.tran@zoi.tech>
2025-03-18 20:08:28 +0700

    added beautiful 404 page

--- src/pages/error/PageNotFound.tsx
+++ src/pages/error/PageNotFound.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Button, Typography, Box } from "@mui/material";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import { useNavigate } from "react-router-dom";
+import './PageNotFound.scss'; // Import SCSS styles
 
 const PageNotFound: React.FC = () => {
-  return <div>Page Not Found</div>;
+  const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <Box className="page-not-found">
+      <Box className="content">
+        <ErrorOutlineIcon className="error-icon" />
+        <Typography variant="h1" className="title">
+          404
+        </Typography>
+        <Typography variant="h4" className="description">
+          Whoops! We couldn't find the page you're looking for.
+        </Typography>
+        <Button variant="contained" className="home-button" onClick={handleGoHome}>
+          Take Me Home
+        </Button>
+      </Box>
+    </Box>
+  );
 };
 
 export default PageNotFound;
